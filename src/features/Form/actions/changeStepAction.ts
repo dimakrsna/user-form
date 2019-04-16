470404d2aa7b8a7f0c6e57f66f6d35a704d479af
dimakrsna <dimakrsna@gmail.com>
2019-04-16 20:57:06 +0300

    responsive markup

--- src/features/Form/actions/changeStepAction.ts
+++ src/features/Form/actions/changeStepAction.ts
@@ -1,6 +1,6 @@
 import { CURRENT_STEP } from './../../../store/consts'
 
-export const changeStepAction = (value: number = 1) => {
+export const changeStepAction = (value: number) => {
     return {
         type: CURRENT_STEP,
         payload: value
